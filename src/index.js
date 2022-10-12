5c32631ceb686c7bdd5feb14f222c312831c3bbf
rahulravidas024 <rahulravidas024@gmail.com>
2022-10-12 22:59:55 +0530

    updateUser API Done

--- src/index.js
+++ src/index.js
@@ -2,28 +2,25 @@ const express = require('express')
 const route = require("./routes/route")
 const mongoose = require('mongoose')
 const multer = require('multer')
-const {AppConfig}= require('aws-sdk');
+const { AppConfig } = require('aws-sdk')
 //const bcrypt = require('brypt')
 
 const app = express()
-
-
+app.use(express.json())
 app.use(multer().any())
 
-
-app.use(express.json());
-
-mongoose.connect("mongodb+srv://sagar:NR489Xvqr9MVCuEZ@cluster0.qks1ndl.mongodb.net/group66Database",{
-    useNewUrlParser:true
+mongoose.connect("mongodb+srv://sagar:NR489Xvqr9MVCuEZ@cluster0.qks1ndl.mongodb.net/group66Database", {
+    useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
-
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
 app.use("/", route)
 
-
+app.use(function (req, res) {
+    return res.status(400).send({ status: false, message: "Path Not Found" })
+})
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
-});
+})
--- src/validations/validation.js
+++ src/validations/validation.js
@@ -1,68 +1,105 @@
 const mongoose = require('mongoose')
+
 // //-------------------------request body validation------------------------------------
 
-// const isValidRequestBody = function (requestBody) {
-//     return Object.keys(requestBody).length > 0;
-//   };
-// //----------------------------email validation-----------------------------------------
+const isValidObjectId = function (objectId) {
+    return mongoose.Types.ObjectId.isValid(objectId)
+}
 
-// const isValidEmail = function (value) {
-//     if (/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(value)) return true;
-//     return false;
-//   };
+const isValidRequestBody = function (requestBody) {
+    return Object.keys(requestBody).length > 0
+}
 
-// //---------------------------password validation---------------------------------------
-//   const isValidPassword = function (value) {
-//     if (
-//       /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,15}$/.test(value)
-//     )
-//       return true;
-//     return false;
-//   };
+const isValidObject = function (value) {
+    return (typeof value === 'object')
+}
 
-  const isValid = function (value) {
-    
-    if (typeof value === "undefined" || typeof value === null) return false;
-    if (typeof value === "string" && value.trim().length === 0) return false;
-    return true;
-};
-
-const isValidNumber = function (value) {
-    const noNumber = /^(\+91[\-\s]?)?[0]?(91)?[6-9]\d{9}$/g
-    if (typeof value !== 'string') return false
-    if (noNumber.test(value) === false) return false
+const isValid = function (value) {
+    if (typeof value === "undefined" || value === null) return false
+    if (typeof value === "string" && value.trim().length === 0) return false
     return true
 }
 
-
-const isValidEmail = function (value) {
-    let mailFormat = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/
-    // Checking if the inputted email id perfectely formatted or not
-    if (!(value.match(mailFormat))) return false
+const isValidName = function (value) {
+    const regex = /^[a-zA-Z ]{2,30}$/
+    if (typeof value !== "string") return false
+    if (regex.test(value) === false) return false
     return true
-
 }
 
-const isValidRequestBody = function (requestbody) {
-    return Object.keys(requestbody).length > 0;
+const isValidEmail = function (email) {
+    const regex = /^\s*[a-zA-Z0-9]+([\.\-\_\+][a-zA-Z0-9]+)*@[a-zA-Z]+([\.\-\_][a-zA-Z]+)*(\.[a-zA-Z]{2,3})+\s*$/
+    return regex.test(email)
 }
 
-const isValidObjectId = function (ObjectId) {
-    return mongoose.Types.ObjectId.isValid(ObjectId)
+const isValidPhone = function (mobileNumber) {
+    const regex = /^(\+91[\-\s]?)?[0]?(91)?[6789]\d{9}$/
+    return regex.test(mobileNumber)
+}
+
+const isValidPassword = function (pass) {
+    let regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,15}$/
+    return regex.test(pass)
 }
 
 const isValidPincode = function (value) {
-    let pinCodeValidation = /^[0-9]{6}$/;
-    if (!(pinCodeValidation.test(value))) return false
+    const regex = /^[0-9]{6}$/
+    return regex.test(value)
+}
+
+function isValidImage(icon) {
+    const ext = [".jpg", ".jpeg", ".bmp", ".gif", ".png", ".svg"]
+    return ext.some((el) => icon.endsWith(el))
+}
+
+const hashPassword = async (password) => {
+    const hash = await bcrypt.hash(password, 10)
+    return hash
+    // console.log(await bcrypt.compare(password, hash))
+}
+
+const isSize = function (title) {
+    //console.log(['Mr','Mrs','Miss'].includes(title));
+    return ["S", "XS", "M", "X", "L", "XXL", "XL"].includes(title.toUpperCase())
+}
+
+const isValidPrice = function (value) {
+    if (/^\d+(\.\d{1,2})?$/.test(value)) return true
+    return false
+};
+
+const isValidNum = function (value) {
+    if (!/^[0-9]+$/.test(value)) {
+        return false
+    }
     return true
 }
 
-const isValidPassword = function(value){
-    let passwordPattern = /^[a-zA-Z0-9!@#$%&*]{8,15}$/;
-        if (!(passwordPattern.test(value))) return false
-        return true
+const isValidBoolean = function (value) {
+    return value === "true" || value === "false"
+}
+
+const isValidSize = (Arr) => {
+    let newArr = []
+    if (Arr.length === 0) {
+        return false
+    }
+    let brr = Arr[0].split(",")
+    for (let i = 0; i < brr.length; i++) {
+        if (
+            !["S", "XS", "M", "X", "L", "XXL", "XL"].includes(brr[i].toUpperCase())
+        ) {
+            return false
+        }
+        newArr.push(brr[i].toUpperCase())
+    }
+    return newArr
+}
+
+const isValidStatus = function (status) {
+    let enumArr = ["pending", "completed", "cancelled"]
+    return enumArr.includes(status); // returns a boolean
 }
 
 
-module.exports = { isValid, isValidEmail, isValidNumber, isValidRequestBody, isValidObjectId, isValidPincode,isValidPassword }
-  // module.exports = {isValidRequestBody,isValidEmail,isValidPassword}
+module.exports = { isValidObjectId, isValidRequestBody, isValidObject, isValid, isValidName, isValidEmail, isValidPhone, isValidPassword, isValidPincode, isValidImage, hashPassword, isSize, isValidPrice, isValidNum, isValidBoolean, isValidSize, isValidStatus }
